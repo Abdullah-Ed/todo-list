9e3c9dc9b8c9c5fb30c0dec8f63e49f3677c9843
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-08-28 22:00:46 +0200

    Add edit option

--- src/UI.js
+++ src/UI.js
@@ -1,6 +1,6 @@
 import { TodosArrays } from "./crateTodo";
 
-class TodoManager {
+class TodoRenderer {
   static renderTodo(todo, index) {
     return `
       <div class="todo-container">
@@ -8,26 +8,86 @@ class TodoManager {
         <p>${todo.description}</p>
         <p>${todo.dueDate}</p>
         <p>${todo.priority}</p>
-        <img class="delete-icon" data-index="${index}" src="../dist/images/delete.svg" alt="">
+        <img class="edit-icon" data-edit-index="${index}" src="../dist/images/edit.svg" alt="Edit icon">
+        <img class="delete-icon" data-delete-index="${index}" src="../dist/images/delete.svg" alt="Delete icon">
       </div>
     `;
   }
+}
 
-  static loadMainTodos() {
-    const content = document.querySelector('.content');
-    content.innerHTML = '';
+class TodoManager {
+  static content = document.querySelector('.content');
+  static currentIndex = null;
 
-    TodosArrays.allTodosArray.forEach((todo, index) => {
-      content.innerHTML += this.renderTodo(todo, index);
+  static initialize() {
+    this.content.addEventListener('click', (event) => {
+      if (event.target.classList.contains('edit-icon')) {
+        const index = event.target.getAttribute('data-edit-index');
+        this.editTodo(index);
+      } else if (event.target.classList.contains('delete-icon')) {
+        const index = event.target.getAttribute('data-delete-index');
+        this.deleteTodo(index);
+      }
     });
   }
 
-  static deleteTodo() {
-    const content = document.querySelector('.content');
-    content.addEventListener('click', (event) => {
-      const index = event.target.getAttribute('data-index');
-      TodosArrays.allTodosArray.splice(index, 1);
-      this.loadMainTodos();
+  static renderTodos() {
+    this.content.innerHTML = TodosArrays.allTodosArray.map((todo, index) => {
+      return TodoRenderer.renderTodo(todo, index);
+    }).join(''); // Join the array to form a single HTML string
+  }
+
+  static deleteTodo(index) {
+    TodosArrays.allTodosArray.splice(index, 1);
+    this.renderTodos();
+  }
+
+  static editTodo(index) {
+    if (document.querySelector('.edited')) {
+      return;
+    }
+  
+    const todo = TodosArrays.allTodosArray[index];
+    
+    this.content.innerHTML += `
+      <div class="form-container edited">
+        <form id="edit-form">
+          <label for="title">Title</label>
+          <input type="text" class='title' name="title" id="title" value="${todo.title}" required>
+      
+          <label for="description">Description</label>
+          <textarea class='description' name="description" id="description">${todo.description}</textarea>
+      
+          <label for="dueDate">Due Date</label>
+          <input type="date" class='dueDate' name="dueDate" id="dueDate" value="${todo.dueDate}" required>
+      
+          <label for="priority">Priority</label>
+          <select name="priority" class='priority' id="priority" required>
+            <option value="high" ${todo.priority === 'high' ? 'selected' : ''}>High</option>
+            <option value="medium" ${todo.priority === 'medium' ? 'selected' : ''}>Medium</option>
+            <option value="low" ${todo.priority === 'low' ? 'selected' : ''}>Low</option>
+          </select>
+          <button type="button" id="save-button">Save</button>
+        </form>
+      </div>
+    `;
+    
+    const editForm = document.getElementById('edit-form');
+    const saveButton = document.getElementById('save-button');
+    saveButton.addEventListener('click', () => {
+      if (!editForm.checkValidity()) {
+        ed.reportValidity();
+        return;
+      }
+      // Handle form submission here and update the TodosArrays
+      const updatedTodo = {
+        title: document.querySelector('.title').value,
+        description: document.querySelector('.description').value,
+        dueDate: document.querySelector('.dueDate').value,
+        priority: document.querySelector('.priority').value,
+      };
+      TodosArrays.allTodosArray[index] = updatedTodo;
+      this.renderTodos();
     });
   }
 }
