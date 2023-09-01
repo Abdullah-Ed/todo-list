89cc33e367d0f14b97c66f983b1d5b198ad1f108
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-09-01 17:09:27 +0200

    Add a form to add projects

--- src/FormHandel.js
+++ src/FormHandel.js
@@ -1,28 +1,62 @@
-import { TodosArrays,Todos } from "./crateTodo";
-import{TodoManager} from "./UI"
-
+import { TodosArrays, Todos } from "./createTodo";
+import { TodoManager } from "./UI";
 
 class FormHandler {
-  static createProject(event) {
+  static projectArray = [];
+
+  static createTodo = (event) => {
     event.preventDefault();
-    const form = event.target.closest('form'); 
+    const form = event.target.closest("form");
 
-    if (!form.checkValidity()) {
-      form.reportValidity();
-      return;
-    }
-    // Use destructuring to extract form values
-    const { title, description, dueDate, priority } = form.elements;
+    if (!this.validateForm(form)) return;
 
-    const newTodo = new Todos(title.value, description.value, dueDate.value, priority.value);
-    
+    const newTodo = this.createTodoFromForm(form);
+
     TodosArrays.addToArray(newTodo);
     console.log(TodosArrays.allTodosArray);
     TodoManager.renderTodos();
   }
+
+  static validateForm(form) {
+    if (!form.checkValidity()) {
+      form.reportValidity();
+      return false;
+    }
+    return true;
+  }
+
+  static createTodoFromForm(form) {
+    const { title, description, dueDate, priority } = form.elements;
+    return new Todos(title.value, description.value, dueDate.value, priority.value);
+  }
+
+  static createProject = (event) => {
+    event.preventDefault();
+    const form = event.target.closest("form");
+
+    if (!this.validateForm(form)) return;
+
+    const input = document.querySelector('#new-project').value;
+    
+    if (this.projectArray.includes(input)) {
+      alert('Please enter a new project name.');
+      return;
+    }
+  
+    this.projectArray.push(input);
+    this.addLi(input);
+    
+    form.reset();
+  }
+
+  static addLi(projectName) {
+    const projectDiv = document.querySelector('.project-container');
+    const li = document.createElement('li');
+    li.textContent = projectName;
+    li.classList.add('project')
+    projectDiv.appendChild(li);
+  }
 }
 
-
-
-
 export default FormHandler;
+
