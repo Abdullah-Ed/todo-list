e8e74875e6a3e3be1dceababf9b3ae77524ebbf6
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-09-01 17:10:29 +0200

    Add add click even for projects button

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import FormHandler from './FormHandel'
 import { TodoManager } from './UI';
 
 
-const addProjectBtn = document.querySelector('.add-project-btn');
-addProjectBtn.addEventListener('click', FormHandler.createProject);
+const addProjectBtn = document.querySelector('.add-task-btn');
+addProjectBtn.addEventListener('click', FormHandler.createTodo);
 
 TodoManager.initialize();
 TodoManager.renderTodos();
+
+const createProjectBtn =document.querySelector('.project-btn');
+createProjectBtn.addEventListener('click',FormHandler.createProject)
