db3c49b5fde855d348a17f7ba8bff16ea101e091
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-09-14 00:02:02 +0200

    Refactor the code to accept tabs

--- src/UI.js
+++ src/UI.js
@@ -4,6 +4,8 @@ import format from "date-fns/format";
 import FormHandler from "./FormHandel";
 
 class TodoRenderer {
+  static currentArray = TodosArrays.allTodosArray
+
   static renderTodo(todo, index) {
     return `
       <div class="todo-container">
@@ -11,8 +13,8 @@ class TodoRenderer {
         <p>${todo.description}</p>
         <p>${format(new Date(todo.dueDate),"dd/MM/yyyy")}</p>
         <p>${todo.priority}</p>
-        <img class="edit-icon" data-edit-index="${index}" src="./images/edit.svg" alt="Edit icon">
-        <img class="delete-icon" data-delete-index="${index}" src="./images/delete.svg" alt="Delete icon">
+        <img class="edit-icon" data-edit-id="${todo.id}" src="./images/edit.svg" alt="Edit icon">
+        <img class="delete-icon" data-delete-id="${todo.id}" src="./images/delete.svg" alt="Delete icon">
       </div>
     `;
   }
@@ -25,75 +27,137 @@ class TodoManager {
   static initialize() {
     this.content.addEventListener('click', (event) => {
       if (event.target.classList.contains('edit-icon')) {
-        const index = event.target.getAttribute('data-edit-index');
-        this.editTodo(index);
+        const id = event.target.getAttribute('data-edit-id');
+        this.editTodo(id);
       } else if (event.target.classList.contains('delete-icon')) {
-        const index = event.target.getAttribute('data-delete-index');
-        this.deleteTodo(index);
+        const id = event.target.getAttribute('data-delete-id');
+        this.deleteTodo(id);
       }
     });
   }
 
-  static renderTodos() {
-    this.content.innerHTML = TodosArrays.allTodosArray.map((todo, index) => {
+  static renderTodosOnPage(currentArray) {
+    this.content.innerHTML = currentArray.map((todo, index) => {
       return TodoRenderer.renderTodo(todo, index);
     }) 
-    Dates.addToTodayArray()
-    console.log(Dates.todayTodoArray)
-    Dates.addToCurrentWeekArray();
-    console.log(Dates.currentWeekTodoArray)
   }
 
-  static deleteTodo(index) {
-    TodosArrays.allTodosArray.splice(index, 1);
-    this.renderTodos();
+  static deleteTodo(id) {
+    let indexInAllTodos = 0;
+    TodosArrays.allTodosArray.forEach((todo, currentIndex)=> {
+        if(todo.id == id){
+            indexInAllTodos = currentIndex
+            console.log(currentIndex)
+            TodosArrays.allTodosArray.splice(indexInAllTodos, 1);
+
+            Dates.updateDatesArray()
+            TabHandler.updateCurrentArray()
+            this.renderTodosOnPage(TodoRenderer.currentArray)
+        }
+    })
+}
+
+static editTodo(id) {
+  if (document.querySelector('.edited')) {
+    return;
   }
 
-  static editTodo(index) {
-    if (document.querySelector('.edited')) {
-      return;
-    }
+  const indexInAllTodos = TodosArrays.allTodosArray.findIndex(todo => todo.id == id);
+  const todo = TodosArrays.allTodosArray[indexInAllTodos];
   
-    const todo = TodosArrays.allTodosArray[index];
+  this.content.innerHTML += `
+    <div class="form-container edited">
+      <form id="edit-form">
+        <label for="title">Title</label>
+        <input type="text" class='title' name="title" id="title" value="${todo.title}" required>
     
-    this.content.innerHTML += `
-      <div class="form-container edited">
-        <form id="edit-form">
-          <label for="title">Title</label>
-          <input type="text" class='title' name="title" id="title" value="${todo.title}" required>
-      
-          <label for="description">Description</label>
-          <textarea class='description' name="description" id="description">${todo.description}</textarea>
-      
-          <label for="dueDate">Due Date</label>
-          <input type="date" class='dueDate' name="dueDate" id="dueDate" value="${todo.dueDate}" required>
-      
-          <label for="priority">Priority</label>
-          <select name="priority" class='priority' id="priority" required>
-            <option value="high" ${todo.priority === 'high' ? 'selected' : ''}>High</option>
-            <option value="medium" ${todo.priority === 'medium' ? 'selected' : ''}>Medium</option>
-            <option value="low" ${todo.priority === 'low' ? 'selected' : ''}>Low</option>
-          </select>
-          <button type="button" id="save-button">Save</button>
-        </form>
-      </div>
-    `;
+        <label for="description">Description</label>
+        <textarea class='description' name="description" id="description">${todo.description}</textarea>
     
-    const editForm = document.getElementById('edit-form');
-    const saveButton = document.getElementById('save-button');
-    saveButton.addEventListener('click', () => {
-      if (!FormHandler.validateForm(editForm)) return;
-      // Handle form submission here and update the TodosArrays
-      const updatedTodo = {
-        title: document.querySelector('.title').value,
-        description: document.querySelector('.description').value,
-        dueDate: document.querySelector('.dueDate').value,
-        priority: document.querySelector('.priority').value,
-      };
-      TodosArrays.allTodosArray[index] = updatedTodo;
-      this.renderTodos();
-    });
+        <label for="dueDate">Due Date</label>
+        <input type="date" class='dueDate' name="dueDate" id="dueDate" value="${todo.dueDate}" required>
+    
+        <label for="priority">Priority</label>
+        <select name="priority" class='priority' id="priority" required>
+          <option value="high" ${todo.priority === 'high' ? 'selected' : ''}>High</option>
+          <option value="medium" ${todo.priority === 'medium' ? 'selected' : ''}>Medium</option>
+          <option value="low" ${todo.priority === 'low' ? 'selected' : ''}>Low</option>
+        </select>
+        <button type="button" id="save-button">Save</button>
+      </form>
+    </div>
+  `;
+  
+  const editForm = document.getElementById('edit-form');
+  const saveButton = document.getElementById('save-button');
+  saveButton.addEventListener('click', () => {
+    if (!FormHandler.validateForm(editForm)) return;
+    const updatedTodo = {
+      id: id, // Retain the same ID
+      title: document.querySelector('.title').value,
+      description: document.querySelector('.description').value,
+      dueDate: document.querySelector('.dueDate').value,
+      priority: document.querySelector('.priority').value,
+    };
+    TodosArrays.allTodosArray[indexInAllTodos] = updatedTodo;
+    Dates.updateDatesArray()
+    TabHandler.updateCurrentArray()
+    this.renderTodosOnPage(TodoRenderer.currentArray);
+  });
+}
+}
+
+
+class TabHandler {
+  static emptyContent() {
+    const content = document.querySelector('.content');
+    content.innerHTML = '';
+  }
+
+  static loadHomeTodos() {
+    TabHandler.emptyContent();
+    TodoRenderer.currentArray = TodosArrays.allTodosArray;
+    TodoManager.renderTodosOnPage(TodoRenderer.currentArray);
+  }
+
+  static loadTodayTodos() {
+    TabHandler.emptyContent();
+    TodoRenderer.currentArray = TodosArrays.todayTodoArray;
+    TodoManager.renderTodosOnPage(TodoRenderer.currentArray);
+  }
+
+  static loadWeeksTodos() {
+    TabHandler.emptyContent();
+    TodoRenderer.currentArray = TodosArrays.currentWeekTodoArray;
+    TodoManager.renderTodosOnPage(TodoRenderer.currentArray);
+  }
+
+  static updateCurrentArray(){
+    const currentTab = document.querySelector('.currentProject'); 
+    if(!currentTab)return;
+    switch (currentTab.textContent) { 
+        case 'Home':
+            TodoRenderer.currentArray = TodosArrays.allTodosArray;
+            break;
+        case 'Today':
+            TodoRenderer.currentArray = TodosArrays.todayTodoArray;
+            break;
+        case 'This Week':
+            TodoRenderer.currentArray = TodosArrays.currentWeekTodoArray;
+            break;
+    }
   }
 }
 
-export { TodoManager };
+
+const projects = document.querySelectorAll('li');
+
+function handleClick(event) {
+  projects.forEach(project => project.classList.remove('currentProject'));
+  event.target.classList.add('currentProject');
+}
+
+projects.forEach(project => project.addEventListener('click', handleClick));
+
+
+export { TodoManager, TodoRenderer, TabHandler };
