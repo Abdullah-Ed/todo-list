5a86670b91745b04e614014c58a4dcd486f2fcdc
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-09-19 20:16:07 +0200

    Refactor code

--- src/createTodo.js
+++ src/createTodo.js
@@ -1,3 +1,5 @@
+import {isThisWeek,isToday,parseISO } from "date-fns";
+
 class TodosArrays {
   static allTodosArray = [];
   static todayTodoArray = [];
@@ -9,58 +11,25 @@ class TodosArrays {
 }
 
 class Todos {
-  constructor(title, description, dueDate, priority) {
+  constructor(title, description, dueDate, priority,project) {
     this.id = Date.now()+ Math.random(); // Using a timestamp as a simple unique identifier
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.project = project
   }
 }
 
 class Dates extends TodosArrays {
-  static formatDate(date) {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    return `${year}-${month}-${day}`;
-  }
-
-  static getCurrentDate() {
-    const date = new Date();
-    return this.formatDate(date);
-  }
-
   static addToTodayArray() {
-    const currentDate = this.getCurrentDate();
-    TodosArrays.todayTodoArray = TodosArrays.allTodosArray.filter((todo) => todo.dueDate === currentDate);
-  }
-
-  static getStartOfWeek() {
-    const date = new Date();
-    const dayOfWeek = date.getDay();
-    const startOfWeek = new Date(date);
-    startOfWeek.setDate(date.getDate() - dayOfWeek);
-
-    return this.formatDate(startOfWeek);
+    TodosArrays.todayTodoArray = TodosArrays.allTodosArray.filter((todo) => isToday(parseISO(todo.dueDate)) ==true);
   }
 
   static addToCurrentWeekArray() {
-    const startOfWeek = this.getStartOfWeek();
-    const endOfWeek = this.getEndOfWeek();
-
     TodosArrays.currentWeekTodoArray = TodosArrays.allTodosArray.filter((todo) => {
-      return todo.dueDate >= startOfWeek && todo.dueDate <= endOfWeek;
+      return isThisWeek(parseISO(todo.dueDate)) == true ;
     });
-  }
-
-  static getEndOfWeek() {
-    const date = new Date();
-    const dayOfWeek = date.getDay();
-    const endOfWeek = new Date(date);
-    endOfWeek.setDate(date.getDate() + (6 - dayOfWeek));
-
-    return this.formatDate(endOfWeek);
   }
 
   static updateDatesArray(){
