65c32142c8e9c7d0fa8360f8292835a5813c9c66
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-09-21 23:08:27 +0200

    Add methods to control styles

--- src/UI.js
+++ src/UI.js
@@ -6,18 +6,19 @@ import {FormHandler} from "./FormHandel";
 class TodoRenderer {
   static currentArray = TodosArrays.allTodosArray;
 
-  static renderTodo(todo) {
-    return `
-      <div class="todo-container">
-        <h1>${todo.title}</h1>
-        <p>${todo.description}</p>
-        <p>${format(new Date(todo.dueDate),"dd/MM/yyyy")}</p>
-        <p>${todo.priority}</p>
-        <img class="edit-icon" data-edit-id="${todo.id}" src="./images/edit.svg" alt="Edit icon">
-        <img class="delete-icon" data-delete-id="${todo.id}" src="./images/delete.svg" alt="Delete icon">
-      </div>
-    `;
-  }
+static renderTodo(todo) {
+  const titleId = todo.status ? 'completed' : 'title';
+  return `
+    <div class="todo-container">
+      <h1 class="title" id="${titleId}" data-status-id="${todo.id}">${todo.title}</h1>
+      <p class="details">${todo.description}</p>
+      <p>${format(new Date(todo.dueDate),"dd/MM/yyyy")}</p>
+      <p>${todo.priority}</p>
+      <img class="edit-icon" data-edit-id="${todo.id}" src="./images/edit.svg" alt="Edit icon">
+      <img class="delete-icon" data-delete-id="${todo.id}" src="./images/delete.svg" alt="Delete icon">
+    </div>
+  `;
+ }
 }
 
 class TodoManager {
@@ -32,6 +33,9 @@ class TodoManager {
       } else if (event.target.classList.contains('delete-icon')) {
         const id = event.target.getAttribute('data-delete-id');
         this.deleteTodo(id);
+      }else if (event.target.classList.contains('title')) {
+        const id = event.target.getAttribute('data-status-id');
+        this.changeStatus(id)
       }
     });
   }
@@ -41,6 +45,25 @@ class TodoManager {
       return TodoRenderer.renderTodo(todo, index);
     }) 
   }
+
+  static changeStatus(id){
+    let indexInAllTodos = 0; 
+    TodosArrays.allTodosArray.forEach((todo, currentIndex)=> {
+      if(todo.id == id){
+        indexInAllTodos = currentIndex;
+        console.log('Current Status:', todo.status);
+        todo.status = !todo.status; // Toggle the status
+        console.log('New Status:', todo.status);
+  
+          ArraysManager.updateDatesArray();
+          TabHandler.updateCurrentArray();
+          
+          this.renderTodosOnPage(TodoRenderer.currentArray);
+       }
+    })
+  }
+  
+
 
   static deleteTodo(id) {
     let indexInAllTodos = 0;
@@ -58,6 +81,11 @@ class TodoManager {
     })
 }
 
+static cancelEdit(event){
+  event.preventDefault();
+  document.querySelector('.edited').remove();
+}
+
 static editTodo(id) {
   if (document.querySelector('.edited')) {
     return;
@@ -65,12 +93,11 @@ static editTodo(id) {
 
   const indexInAllTodos = TodosArrays.allTodosArray.findIndex(todo => todo.id == id);
   const todo = TodosArrays.allTodosArray[indexInAllTodos];
-  
   this.content.innerHTML += `
     <div class="form-container edited">
       <form id="edit-form">
         <label for="title">Title</label>
-        <input type="text" class='title' name="title" id="title" value="${todo.title}" required>
+        <input type="text" class='title-input' name="title" id="title" value="${todo.title}" required>
     
         <label for="description">Description</label>
         <textarea class='description' name="description" id="description">${todo.description}</textarea>
@@ -84,22 +111,26 @@ static editTodo(id) {
           <option value="medium" ${todo.priority === 'medium' ? 'selected' : ''}>Medium</option>
           <option value="low" ${todo.priority === 'low' ? 'selected' : ''}>Low</option>
         </select>
-        <button type="button" id="save-button">Save</button>
+        <button id="save-button">Save</button>
+        <button class="cancel-edit-form">Cancel</button>
       </form>
     </div>
   `;
-  
+  const cancelEditBtn = document.querySelector('.cancel-edit-form');
+  cancelEditBtn.addEventListener('click', this.cancelEdit.bind(this));
+
   const editForm = document.getElementById('edit-form');
   const saveButton = document.getElementById('save-button');
   saveButton.addEventListener('click', () => {
     if (!FormHandler.validateForm(editForm)) return;
     const updatedTodo = {
       id: id, // Retain the same ID
-      title: document.querySelector('.title').value,
+      title: document.querySelector('.title-input').value,
       description: document.querySelector('.description').value,
       dueDate: document.querySelector('.dueDate').value,
       priority: document.querySelector('.priority').value,
       project: todo.project,
+      status: todo.status,
     };
     TodosArrays.allTodosArray[indexInAllTodos] = updatedTodo;
     ArraysManager.updateDatesArray()
@@ -185,6 +216,42 @@ class TabHandler {
 
 }
 
+class FormControl{
+  static projectFormContainer = document.querySelector('.project-form-container');
+  static formContainer = document.querySelector('#form-container');
+
+  static showProjectForm(event){
+    event.preventDefault();
+    this.projectFormContainer.style.display="block";
+  }
+
+  static hideProjectForm(event){
+      event.preventDefault();
+      this.projectFormContainer.style.display="none"
+  }
+
+  static showForm(event){
+    event.preventDefault();
+    this.formContainer.style.display="block";
+  }
+
+  static hideForm(event){
+    event.preventDefault();
+    this.formContainer.style.display="none";
+  }
+
+  static styleControl(){
+    const showProjectBtn = document.querySelector('.show-project-form');
+    const hideProjectBtn = document.querySelector('.cancel-project-form');
+    const showFromBtn = document.querySelector('.show-form');
+    const hideFromBtn = document.querySelector('.cancel');
+
+    showProjectBtn.addEventListener('click',this.showProjectForm.bind(this));
+    hideProjectBtn.addEventListener('click',this.hideProjectForm.bind(this));
+    showFromBtn.addEventListener('click',this.showForm.bind(this));
+    hideFromBtn.addEventListener('click',this.hideForm.bind(this));
+  }
+} 
 
 
-export { TodoManager, TodoRenderer, TabHandler };
+export { TodoManager, TodoRenderer, TabHandler,FormControl };
